0181873930b041fd0b9946b391d5a0b107d96384
Felivander <feli.vander@gmail.com>
2024-11-05 15:47:29 -0300

    Finished console game

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const rock = 1;
-const paper = 2;
-const scissors = 3;
-
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
 
@@ -36,8 +32,61 @@ function getHumanChoice() {
   }
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(computerChoice);
-console.log(humanChoice);
+function playGame() {
+  function playRound(human, computer) {
+    if (human == "rock" && computer == "paper") {
+      computerScore++;
+    }
+    if (human == "paper" && computer == "rock") {
+      humanScore++;
+    }
+    if (human == "paper" && computer == "scissors") {
+      computerScore++;
+    }
+    if (human == "scissors" && computer == "paper") {
+      humanScore++;
+    }
+    if (human == "rock" && computer == "scissors") {
+      humanScore++;
+    }
+    if (human == "scissors" && computer == "rock") {
+      computerScore++;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    console.log("Round number:", i + 1);
+
+    playRound(humanChoice, computerChoice);
+
+    console.log("Human score: ", humanScore);
+    console.log("Computer score: ", computerScore);
+    if (humanScore > computerScore) {
+      console.log("Human wins the round!");
+    } else if (computerScore > humanScore) {
+      console.log("Computer wins the round!");
+    } else {
+      console.log("It's a Tie!");
+    }
+  }
+}
+
+playGame();
+
+console.clear();
+console.log("Final Scores:");
+console.log("Computer: ", computerScore);
+console.log("Human: ", humanScore);
+
+if (humanScore > computerScore) {
+  console.log("Human wins!");
+} else if (computerScore > humanScore) {
+  console.log("Computer wins");
+} else {
+  console.log("It's a Tie!");
+}
